7fb5934d550234707aebc55e08d04949f658572a
Usuario <Usuario@DESKTOP-PARS1KU>
2024-09-20 19:20:49 +0200

    Ejercicio 5 terminado

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,9 @@
+package exercise5;
+
+public class Exercise5 {
+	public static void main(String[] args) {
+		System.out.println("Juan Antonio");
+		System.out.println("Calle Joaquin Turina 2 2B, Brenes, Sevilla");
+		System.out.println("640997224");
+	}
+}
